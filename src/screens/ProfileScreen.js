134b927ed81cb11e1af4bb851ea43199c39eca84
Shrey <shreybirmiwal@gmail.com>
2021-04-19 10:16:42 -0500

    Profile Screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,28 +1,57 @@
-import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import React, {useContext} from 'react';
+import styled from 'styled-components';
+
+import {UserContext} from '../context/UserContext';
+import {FirebaseContext} from '../context/FirebaseContext';
+
+import Text from '../componenets/Text';
 
 export default ProfileScreen = () => {
+    const [user, setUser] = useContext(UserContext);
+    const firebase = useContext(FirebaseContext);
 
     return (
+        <Container>
+            <ProfilePhotoContainer>
+                <ProfilePhoto 
+                source={
+                    user.profilePhotoUrl === "default"
+                    ? require("../../assets/defaultprofile.png")
+                    : {uri: user.profilePhotoUrl}
+                    }>
+                </ProfilePhoto>
+            </ProfilePhotoContainer>
 
-        <View style = {styles.container}>
+            <Text medium bold margin="16px 0 32px 0">
+                    {user.username}
+            </Text>
 
-            <Text> Profile Screen</Text>
-
-        </View>
-
+            <Logout> 
+                <Text large bold>Log Out</Text>
+            </Logout>
+        </Container>
     );
 
+};
 
-}
+const Container = styled.View`
 
-const styles = StyleSheet.create({
+    align-items: center;
+    margin-top: 64px;
+    flex: 1;
+`;
+const ProfilePhotoContainer = styled.View`
+    shadow-opacity: .6;
+    shadow-radius: 30px;
+    shadow-color: #222222;
+`;
+const ProfilePhoto = styled.Image`
 
-    container: {
+    width: 128px;
+    height: 128px;
+    border-radius: 64px;
 
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-
-})
+`;
+const Logout = styled.TouchableOpacity`
+    margin-bottom: 32px;
+`;
